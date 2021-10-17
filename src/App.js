b3f4397b8d205e24eeee2782b3a056abe1d5e157
hjang212 <hjjang8812@gmail.com>
2021-10-17 23:08:17 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Header from "./components/Header"
-
+import GlobalStyle from "./components/GlobalStyles"
 function App() {
   return (
-    <>
+    <> <GlobalStyle />
       <Header />
       Hello react
     </>
